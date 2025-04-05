0f7953ce116a6a7723b9a733864478a80b77c8a1
godgel <83522575+godgel@users.noreply.github.com>
2025-04-05 16:14:12 +0900

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+
+"""
+Program Name : hello.py  ( print python text in the Linux. )
+
+Description : Prints Text in terminal.
+
+Name : NS, Jo.
+
+Date : 2025.04.01
+
+"""
+
+print("Hello_Python_In_The_Ubuntu_Server")
+
+
---
+++ hello2.py
@@ -0,0 +1,27 @@
+
+"""
+Program Name : hello.py  ( print python text in the Linux. )
+
+Description : Prints Text in terminal.
+
+Name : NS, Jo.
+
+Date : 2025.04.01
+
+"""
+
+print("Hello_Python_In_The_Ubuntu_Server")
+
+def addNumbers(left_num, right_num) : 
+    
+    sumNumbers = left_num + right_num  
+    
+    return sumNumbers
+
+""" Return The Result Of Two Numbers"""
+
+receiveNumbers = addNumbers(777, 111) 
+''' Assign the result to left_var after calling function'''
+
+print(receiveNumbers)
+
